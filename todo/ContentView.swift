80c524a86a3b487f7905356d6c0208c02ba811cd
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-10-14 10:33:19 +0200

    Refactored Item.swift file to TodoItem

--- todo/ContentView.swift
+++ todo/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var todoItemName:String = ""
+    @State private var todoItems:[TodoItem] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                
+                HStack{
+                    TextField(
+                        "New Todo...",
+                        text:$todoItemName
+                    ).textFieldStyle(RoundedBorderTextFieldStyle()).border(Color.blue, width: 2)
+                    
+                    Button(action: {
+                                guard !self.todoItemName.isEmpty else {return}
+                                self.todoItems.append(TodoItem(name: self.todoItemName))
+                                self.todoItemName = ""
+                        },
+                           label: {
+                                Image(systemName:"text.badge.plus")
+                        }
+                    ).padding(.leading,5)
+                }.padding()
+                
+                List{
+                    ForEach(self.todoItems){ todoItem in
+                        Text("\(todoItem.name)")
+                    }
+                }
+                
+            }
+            .navigationTitle("Todo List")
         }
-        .padding()
+    }
+    private func save(){
+        UserDefaults.standard.set(
+            try? PropertyListEncoder().encode(self.todoItems),
+            forKey: "myTodoItemsKey"
+        )
+    }
+    private func load(){
+        if let todoItemsData = UserDefaults.standard.value(forKey: "myTodoItemsKey") as? Data {
+            if let todoItemsList = try? PropertyListDecoder().decode(Array<TodoItem>.self,from:todoItemsData){
+                self.todoItems = todoItemsList
+            }
+        }
     }
 }
 
