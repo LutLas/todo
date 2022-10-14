0a376fdca8cedd45a25c811b444852e139208e9d
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-10-14 07:55:11 +0200

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Lutaka Muyoba Chihota on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
